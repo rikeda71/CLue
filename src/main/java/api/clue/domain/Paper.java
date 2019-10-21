2dafd3caeb0585a5037b532d36e31d09cf267e5d
Ryuya Ikeda <rikeda71@gmail.com>
2019-10-21 15:10:53 +0900

    Add: a domain class of research papers

---
+++ src/main/java/api/clue/domain/Paper.java
@@ -0,0 +1,36 @@
+package api.clue.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class Paper {
+
+    private int id;
+
+    @JsonProperty
+    private int year;
+
+    private String label;
+
+    @JsonProperty
+    private String task;
+
+    private String session;
+
+    @JsonProperty
+    private String title;
+
+    @JsonProperty
+    private String url;
+
+    @JsonProperty
+    private String introduction;
+
+    private String lang;
+
+    @JsonProperty
+    private String conference;
+}
