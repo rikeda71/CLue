cb75cd797aa5db83f4916a8512f565599afd442c
Ryuya Ikeda <rikeda71@gmail.com>
2019-10-23 00:13:25 +0900

    insert papers with authors relation

--- src/main/java/api/clue/service/PaperService.java
+++ src/main/java/api/clue/service/PaperService.java
@@ -1,6 +1,8 @@
 package api.clue.service;
 
+import api.clue.domain.Author;
 import api.clue.domain.Paper;
+import api.clue.mapper.AuthorMapper;
 import api.clue.mapper.PaperMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,12 @@ public class PaperService {
 
     @Autowired
     private PaperMapper paperMapper;
+
+    @Autowired
+    private AuthorMapper authorMapper;
+
+    @Autowired
+    private PWAService pwaService;
 
     public Paper findById(Integer id) {
         Paper paper = paperMapper.findById(id);
@@ -28,7 +36,7 @@ public class PaperService {
     public void insertPaper(Integer year, String label, String task, String title,
                             String url, String introduction, String lang, String conference, List<String> authors) {
         Paper paper = new Paper();
-        paper.setYear(year.intValue());
+        paper.setYear(year);
         paper.setLabel(label);
         paper.setTask(task);
         paper.setTitle(title);
@@ -38,6 +46,24 @@ public class PaperService {
         paper.setConference(conference);
 
         paperMapper.insertPaper(paper);
+
+        // 著者IDを取得．なければ新たに著者をinsertしてそのIDを取得
+        Author author;
+        List<Author> tmpAuthors;
+        for (String authorName : authors) {
+            tmpAuthors = authorMapper.findAuthor(authorName);
+            if (tmpAuthors.size() > 0) {
+                author = tmpAuthors.get(0);
+            }
+            else {
+                author = new Author();
+                author.setName(authorName);
+                authorMapper.insertAuthor(author);
+            }
+            // 論文を書いた著者名を保存
+            pwaService.insertPWA(author, paper);
+        }
+
     }
 
     public void updatePaper(int id, Integer year, String label, String task, String title,
