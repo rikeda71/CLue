5f73e927add5761dd61ce4dc01d77e6764329b78
Ryuya Ikeda <rikeda71@gmail.com>
2019-10-22 15:44:49 +0900

    Add: db settings

---
+++ docker/db/sql/4_create_paper_written_author_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS paper_written_author(
+`author_id` int(5) NOT NULL,
+`paper_id` int(4) NOT NULL,
+PRIMARY KEY (`author_id`, `paper_id`),
+FOREIGN KEY (`author_id`)
+    REFERENCES `authors` (`id`)
+    ON DELETE RESTRICT ON UPDATE RESTRICT,
+FOREIGN KEY (`paper_id`)
+    REFERENCES `papers` (`id`)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
