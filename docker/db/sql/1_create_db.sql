6967a780f5e6562e76805fc720072b48331f7941
Ryuya Ikeda <rikeda71@gmail.com>
2020-06-01 00:00:50 +0900

    close #10   organize temporary data

--- docker/db/sql/1_create_db.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE IF NOT EXISTS research_paper_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE research_paper_db;
--- docker/db/sql/3_create_authors_table.sql
+++
@@ -1,7 +0,0 @@
-USE research_paper_db;
-CREATE TABLE IF NOT EXISTS `authors` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `name` varchar(50),
-  PRIMARY KEY (`id`,`name`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB;
