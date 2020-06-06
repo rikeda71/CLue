4fce2074ca7560095f686f645f9696b7dbeda222
Ryuya Ikeda <rikeda71@gmail.com>
2020-06-06 18:54:37 +0900

    add a baseline of component for header and footer

---
+++ frontend/src/stories/footer/Footer.stories.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("3_organisms", module).add("Footer", () => <Footer />);
