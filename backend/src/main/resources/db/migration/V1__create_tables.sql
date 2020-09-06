5f233c210d83bb4806eb2f710c30a7d3e8fc240c
Ryuya Ikeda <rikeda71@gmail.com>
2020-09-06 14:44:26 +0900

    Add table definitions

--- backend/src/main/resources/db/migration/V1__create_tables.sql
+++ backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -12,18 +12,34 @@ CREATE TABLE IF NOT EXISTS `papers` (
 );
 
 CREATE TABLE IF NOT EXISTS `authors` (
-  `author_id` BIGINT AUTO_INCREMENT PRIMARY KEY ,
-  `name` varchar(50) UNIQUE
+    `author_id` BIGINT AUTO_INCREMENT PRIMARY KEY ,
+    `name` varchar(50) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS `task` (
+    `task_id` int(3) PRIMARY KEY AUTO_INCREMENT,
+    `task_name` varchar(50) NOT NULL UNIQUE,
+    `task_label_name` varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS `conference` (
+    `conference_id` int(3) PRIMARY KEY AUTO_INCREMENT,
+    `conference_name` varchar(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS `year` (
+    `year_id` int(2) PRIMARY KEY AUTO_INCREMENT,
+    `year` int(4) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS `paper_written_author` (
-`author_id` BIGINT NOT NULL,
-`paper_id` BIGINT NOT NULL,
-PRIMARY KEY (`author_id`, `paper_id`),
-FOREIGN KEY (`author_id`)
-    REFERENCES `authors` (`author_id`)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (`paper_id`)
-    REFERENCES `papers` (`paper_id`)
-    ON DELETE CASCADE ON UPDATE CASCADE
+    `author_id` BIGINT NOT NULL,
+    `paper_id` BIGINT NOT NULL,
+    PRIMARY KEY (`author_id`, `paper_id`),
+    FOREIGN KEY (`author_id`)
+        REFERENCES `authors` (`author_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`paper_id`)
+        REFERENCES `papers` (`paper_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
