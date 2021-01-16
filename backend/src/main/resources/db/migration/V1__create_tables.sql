da84b9a9b3e4ebbf420166565de728d1eea7c7d2
YujiOka <s16t215@stu.kagawa-u.ac.jp>
2021-01-16 17:59:57 +0900

    dbの設定を変更

--- backend/src/main/resources/db/migration/V1__create_tables.sql
+++ backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -43,3 +43,12 @@ CREATE TABLE IF NOT EXISTS `paper_written_author` (
         REFERENCES `papers` (`paper_id`)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY ,
+    `name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL UNIQUE,
+    `image_url` varchar(1000) NOT NULL,
+    `password` varchar(200),
+    `user_type` varchar(100) NOT NULL
+);
