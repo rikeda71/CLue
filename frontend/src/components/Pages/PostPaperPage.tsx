b9cb874100315c3cc317907b000e009a7dd1b86d
YujiOka <s16t215@stu.kagawa-u.ac.jp>
2021-03-27 17:57:52 +0900

    フォームを追加．セレクトボックスがうまく実装できず苦戦中

--- frontend/src/components/Pages/PostPaperPage.tsx
+++ frontend/src/components/Pages/PostPaperPage.tsx
@@ -8,15 +8,23 @@ const PostPaperPageStyle = styled.div`
   display: block;
 `;
 
+const ButtonsDiv = styled.div`
+  margin: 10px 0;
+  text-align: center;
+  display: flex;
+`;
+
 const YearForm = styled.input`
   margin: 0 auto;
 `;
 
+const LabelForm = styled.select``;
+
+const TaskForm = styled.select``;
+
 const TitleForm = styled.input``;
 
 const IntroForm = styled.input``;
-
-const LabelForm = styled.select``;
 
 const LangForm = styled.select``;
 
@@ -31,9 +39,10 @@ const PostButton = styled(MainButtonStyle)``;
 // 論文情報を追加するページ
 const PostPaperPage: React.FC = () => {
   const [year, setYear] = useState(0);
+  const [label, setLabel] = useState("nanzoya");
+  const [task, setTask] = useState("");
   const [title, setTitle] = useState("");
   const [intro, setIntro] = useState("");
-  const [label, setLabel] = useState("");
   const [lang, setLang] = useState("");
   const [conf, setConf] = useState("");
   const [authors, setAuthors] = useState([""]);
@@ -43,12 +52,28 @@ const PostPaperPage: React.FC = () => {
     setYear(Number.parseInt(event.target.value));
   };
 
+  const handleLabelFormSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setLabel(event.target.value);
+  };
+  
+  const handleTaskFormSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
+  };
+
   const handleTitleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
   };
 
   const handleIntroFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIntro(event.target.value);
+  };
+
+  const handleLangFormSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLabel(event.target.value);
+  };
+
+  const handleConfFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLabel(event.target.value);
   };
 
   const handleAuthorFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -80,39 +105,58 @@ const PostPaperPage: React.FC = () => {
 
   return (
     <PostPaperPageStyle>
-      <tbody>
+      <ButtonsDiv>
+        <tbody>
         <tr>
-          <td>年</td>
-          <td>
-            <YearForm onChange={handleYearFormChange} />
-          </td>
-        </tr>
-        <tr>
-          <td>タイトル</td>
-          <td>
-            <TitleForm onChange={handleTitleFormChange} />
-          </td>
-        </tr>
-        <tr>
-          <td>イントロダクション</td>
-          <td>
-            <IntroForm onChange={handleIntroFormChange} />
-          </td>
-        </tr>
-        <tr>
-          <td>著者</td>
-          <td>
-            <AuthorForm onChange={handleAuthorFormChange} />
-          </td>
-        </tr>
-        <tr>
-          <td>URL</td>
-          <td>
-            <UrlForm onChange={handleUrlFormChange} />
-          </td>
-        </tr>
-      </tbody>
-      <PostButton onClick={handleClick} />
+            <td>年</td>
+            <td>
+              <YearForm onChange={handleYearFormChange} />
+            </td>
+          </tr>
+          <tr>
+            <td>ラベル</td>
+            <td>
+              <select name="label" onChange={handleLabelFormSelect}>
+                <option value={2019}>
+                  2019
+                </option>
+              </select>
+              {/* <LabelForm onChange={handleLabelFormChange} /> */}
+            </td>
+          </tr>
+          <tr>
+            <td>タスク</td>
+            <td>
+              <TaskForm onChange={handleTaskFormChange} />
+            </td>
+          </tr>
+          <tr>
+            <td>タイトル</td>
+            <td>
+              <TitleForm onChange={handleTitleFormChange} />
+            </td>
+          </tr>
+          <tr>
+            <td>イントロダクション</td>
+            <td>
+              <IntroForm onChange={handleIntroFormChange} />
+            </td>
+          </tr>
+          <tr>
+            <td>著者</td>
+            <td>
+              <AuthorForm onChange={handleAuthorFormChange} />
+            </td>
+          </tr>
+          <tr>
+            <td>URL</td>
+            <td>
+              <UrlForm onChange={handleUrlFormChange} />
+            </td>
+          </tr>
+        </tbody>
+      </ButtonsDiv>
+      <PostButton onClick={handleClick}>追加</PostButton>
     </PostPaperPageStyle>
   );
 };
